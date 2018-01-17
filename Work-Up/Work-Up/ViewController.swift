44b748f2d04365ce39b28957e4b8de0b910e2020
Mirko Pennone <mirko@pennone.org>
2018-01-17 10:32:08 +0100

    tableview

--- Work-Up/Work-Up/ViewController.swift
+++ Work-Up/Work-Up/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sharedFilePath: String?
+    var sharedDefaults: UserDefaults?
+    let fileManager = FileManager.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
