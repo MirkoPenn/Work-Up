9acee96f27c127e09b1f6651e55c94c42dea8d53
Mirko Pennone <mirko@pennone.org>
2018-01-18 20:47:40 +0100

    added timer ring (doesn't work)

--- Work-Up/Work-Up WatchKit Extension/InterfaceController.swift
+++ Work-Up/Work-Up WatchKit Extension/InterfaceController.swift
@@ -10,6 +10,7 @@ import WatchKit
 import WatchConnectivity
 import Foundation
 import CoreData
+import HealthKit
 
 
 class InterfaceController:  WKInterfaceController{
@@ -29,6 +30,23 @@ class InterfaceController:  WKInterfaceController{
     var exercises: [Exercise] = []
     var currentIndex: Int = 0
     var currentSeries: Int = 1
+    
+    var timer: Timer = Timer()
+    var count = 10
+    
+    @IBOutlet var ringActivity: WKInterfaceActivityRing!
+    
+    let summary = HKActivitySummary()
+    var value: Double = 0
+    //deve essere uguale al recupero
+    var goal: Double = 10
+    //    var activeEnergyBurned: HKQuantity
+    //    var activeEnergyBurnedGoal: HKQuantity
+    //    var appleExerciseTime: HKQuantity
+    //    var appleExerciseTimeGoal: HKQuantity
+    //    var appleStandHours: HKQuantity
+    //    var appleStandHoursGoal: HKQuantity
+    
     
     let coreDataContext : NSManagedObjectContext =
     {
@@ -107,6 +125,27 @@ class InterfaceController:  WKInterfaceController{
         
         }
         
+        // set timer for rest
+        
+        //barra rossa per le calorie bruciate
+        summary.activeEnergyBurned = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: value)
+        summary.activeEnergyBurnedGoal = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: goal)
+        
+        //barra verde per il timer e conteggio (utile per il recupero)
+        summary.appleExerciseTime = HKQuantity(unit: HKUnit.minute(), doubleValue: value)
+        summary.appleExerciseTimeGoal = HKQuantity(unit: HKUnit.minute(), doubleValue: goal)
+        
+        //barra blu per il conteggio passi
+        //        summary.appleStandHours = HKQuantity(unit: HKUnit.count(), doubleValue: value)
+        //        summary.appleStandHoursGoal = HKQuantity(unit: HKUnit.count(), doubleValue: goal)
+        
+        
+        ringActivity.setActivitySummary(summary, animated: true)
+        // Configure interface objects here.
+        if(value == goal) {
+            WKInterfaceDevice().play(.notification)
+        }
+        
     }
     
     @IBAction func onClickStart() {
@@ -180,6 +219,7 @@ class InterfaceController:  WKInterfaceController{
                 setTitle("Finished")
             }
         }
+
         
     }
     
@@ -214,11 +254,32 @@ class InterfaceController:  WKInterfaceController{
         return results
     }
     
+    
+    
+    @objc func countdown(sender: Timer) {
+        
+        count = count - 1
+        value = value + 1
+        
+        //aggiorna il ring
+        awake(withContext: self)
+        
+        if count == 0 {
+            //timer si disabilita
+            sender.invalidate()
+            print("now the state is \(sender.isValid)")
+        }
+        
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         
-        print("Will Activate")
         super.willActivate()
+        
+        
+        let clock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown(sender:)), userInfo: nil, repeats: true)
+        
     }
     
     override func didDeactivate() {
